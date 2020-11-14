4fc86ff9c333b1a950365c2b9f5cb004588900db
Grant Beasley <grant592@hotmail.com>
2020-11-14 20:33:32 +0000

    Initial app commit

---
+++ app.py
@@ -0,0 +1,346 @@
+from fv_profile import fvProfile
+import dash_core_components as dcc
+import dash_html_components as html
+import os
+import csv
+import pandas as pd
+import numpy as np
+import dash
+import dash_table
+from dash.dependencies import Input, Output, State
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import base64
+import io
+from collections import OrderedDict
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+def parse_csv(csv_file, filename):
+    """Helper function to parse each individual csv file
+    uploaded by user and process the data prior to graphing"""
+    name, bw, date = filename.replace(".csv", '').split("-")
+    label = f'{name}-{date}'
+    content_type, content_string = csv_file.split(",")
+    decoded = base64.b64decode(content_string)
+    df = pd.read_csv(io.StringIO(decoded.decode('utf-8')), header=None, sep=";")
+    name = " ".join([x.title() for x in name.split("_")])
+    df.columns = ['time', 'data', 'vel', 'lat', 'long']
+    fv = fvProfile(df, name, float(bw))
+    fv.smooth_data()
+    fv.find_peaks()
+    fv.extract_peaks()
+    fv.extract_accel_phase()
+    fv.model_sprint_data()
+    fv.apply_calculations()
+    fv.calculate_params()
+
+    return label, fv.sprints, fv.sprint_dict
+
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.layout = html.Div([
+    dcc.Store(id='memory-store'),
+    html.H1("Force Velocity Profile Dashboard"),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    dcc.Dropdown(
+        id='dropdown-menu',
+        multi=True
+    ),
+    html.Div(
+        id='table1'
+    ),
+    html.Div(
+        [
+            dcc.Graph(
+                id='force-speed-graph',
+                className='six columns' 
+            ),
+            dcc.Graph(
+                id='force-power-graph',
+                className='six columns'
+            ),
+        ],
+        className='row'
+    ),
+    html.Div(
+        [
+            dcc.Graph(
+                id='power-velocity-graph',
+                className='six columns' 
+            ),
+            dcc.Graph(
+                id='horizontal-force-graph',
+                className='six columns' 
+            )
+        ],
+        className='row'
+    )
+])
+
+
+@app.callback(
+    Output('memory-store', 'data'),
+    [Input('upload-data', 'contents')],
+    [State('upload-data', 'filename')])
+def store_sprint_data(csv_files, filenames):
+    master_dict = {}
+    for csv_file, filename in zip(csv_files, filenames):
+        label, sprint_data, sprint_dict = parse_csv(csv_file, filename)
+        master_dict[label] = {}
+        master_dict[label]['sprint_data'] = [df.to_dict(into=OrderedDict) for df in sprint_data]
+        master_dict[label]['sprint_dict'] = sprint_dict
+
+    return master_dict
+
+@app.callback(
+    Output('dropdown-menu', 'options'),
+    [Input('memory-store', 'data')])
+def create_dropdown(data):
+    options = []
+    for key in data.keys():
+        name, date = key.split("-")
+        name = " ".join(name.split("_")).title()
+        sprints = [sprint for sprint in data[key]['sprint_dict'].keys()]
+        print(sprints)
+        for sprint in sprints:
+            key_sprint = "-".join([key,sprint])
+            options.append({'label':f'{name} {date} {sprint}', 'value':key_sprint})
+    print(options)
+    return options
+
+# Need to update the key value to reflect sprint data
+# Need to process the new key value in the following few callbacks
+
+@app.callback(
+    Output('table1', 'children'),
+    [Input('memory-store', 'data'),
+    Input('dropdown-menu','value')])
+def update_datatable(data, keys):
+    tables = []
+    for i,key in enumerate(keys): # can add second key for sprint values
+        key_split = key.split('-')
+        key = "-".join(key_split[:2])
+        data_temp = data[key]['sprint_dict'] # this could look like data[key]['sprint_dict'][key2]
+        df = pd.DataFrame(data_temp).round(2).T
+        print(df)
+        table = dash_table.DataTable(id=f'table_{key}',
+                                     columns=[{"name":k,"id":k} for k in df.columns],
+                                     data=df.to_dict('record'))
+        key = key.split('-')
+        key = f"{key[0].replace('_', ' ').title()} - {key[1]}"
+        tables.append((key,table))
+
+    return [html.Div(
+        [
+            html.H5(key),
+            table
+        ]) for (key, table) in tables
+    ]
+
+
+@app.callback(
+    Output('force-speed-graph','figure'),
+    [Input('memory-store', 'data'),
+     Input('dropdown-menu', 'value')])
+def force_speed_graph(data, keys):
+    fig = make_subplots(specs=[[{"secondary_y":True}]])
+    fig.update_layout(title_text='Force & Speed Profile')
+    fig.update_xaxes(
+        title_text='Time (s)',
+    )
+    fig.update_yaxes(
+        title_text='Velocity (m/s)',
+        range=[0,10]
+    )
+    fig.update_yaxes(
+        title_text='Acceleration (m/s/s)',
+        secondary_y=True,
+        range=[0,10]
+    )
+    for key in keys:
+        key_list = key.split("-")
+        date = key_list[1]
+        key = "-".join(key_list[:2])
+        sprint_id = int(key_list[2][-1])
+        sprint = data[key]['sprint_data'][sprint_id]
+        df = pd.DataFrame(sprint)
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['vel'],
+#                name=f'Vel- {idx} - {date}',
+                name = f'Vel - {key}',
+                mode='markers'
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['predicted_velocity'],
+                name='Predicted Vel'
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['acceleration'],
+                name='Acceleration'
+            ),
+            secondary_y=True
+        )
+
+    return fig
+
+
+@app.callback(
+    Output('force-power-graph','figure'),
+    [Input('memory-store', 'data'),
+     Input('dropdown-menu', 'value')])
+def force_speed_graph(data, keys):
+    fig = make_subplots(specs=[[{"secondary_y":True}]])
+    fig.update_layout(title_text='Force & Power Profile')
+    fig.update_xaxes(
+        title_text='Time (s)',
+    )
+    fig.update_yaxes(
+        title_text='Force (N/kg) and Speed (m/s)',
+        range=[0,10]
+    )
+    fig.update_yaxes(
+        title_text='Power (W/kg)',
+        secondary_y=True,
+        range=[0,20],
+        dtick=4
+    )
+    for key in keys:
+        key_list = key.split("-")
+        date = key_list[1]
+        key = "-".join(key_list[:2])
+        sprint_id = int(key_list[2][-1])
+        sprint = data[key]['sprint_data'][sprint_id]
+        df = pd.DataFrame(sprint)
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['predicted_velocity'],
+                name=f'Vel - {key}',
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['Ftot_kg'],
+                name='Force/kg'
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x = df['time'],
+                y = df['PowerHzt_kg'],
+                name='Power (W/kg)'
+            ),
+            secondary_y=True
+        )
+
+    return fig
+
+
+@app.callback(
+    Output('power-velocity-graph','figure'),
+    [Input('memory-store', 'data'),
+     Input('dropdown-menu', 'value')])
+def force_speed_graph(data, keys):
+    fig = make_subplots(specs=[[{"secondary_y":True}]])
+    fig.update_layout(title_text='Field Sprint Force-Velocity-Power Profile')
+    fig.update_xaxes(
+        title_text='Time (s)',
+    )
+    fig.update_yaxes(
+        title_text='Force (N/kg)',
+        range=[0,8]
+    )
+    fig.update_yaxes(
+        title_text='Power (W/kg)',
+        secondary_y=True,
+        range=[0,16],
+        dtick=4
+    )
+    for key in keys:
+        key_list = key.split("-")
+        date = key_list[1]
+        key = "-".join(key_list[:2])
+        sprint_id = int(key_list[2][-1])
+        sprint = data[key]['sprint_data'][sprint_id]
+        df = pd.DataFrame(sprint)
+        fig.add_trace(
+            go.Scatter(
+                x = df['predicted_velocity'],
+                y = df['Ftot_kg'],
+                name=f'Vel - {key}',
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x = df['predicted_velocity'],
+                y = df['PowerHzt_kg'],
+                name='Power (W/kg)'
+            ),
+            secondary_y=True
+        )
+
+    return fig
+
+@app.callback(
+    Output('horizontal-force-graph','figure'),
+    [Input('memory-store', 'data'),
+     Input('dropdown-menu', 'value')])
+def force_speed_graph(data, keys):
+    fig = go.Figure()
+    fig.update_layout(title_text='Horizontal Force Velocity Profile')
+    fig.update_xaxes(
+        title_text='Velocity (m/s))',
+    )
+    fig.update_yaxes(
+        title_text='Ratio Force (%)',
+    )
+    for key in keys:
+        key_list = key.split("-")
+        date = key_list[1]
+        key = "-".join(key_list[:2])
+        sprint_id = int(key_list[2][-1])
+        sprint = data[key]['sprint_data'][sprint_id]
+        df = pd.DataFrame(sprint)
+        fig.add_trace(
+            go.Scatter(
+                x = df['predicted_velocity'][df['time'] > 0.3],
+                y = df['RF_perc'][df['time'] > 0.3],
+                name=f'Ratio - {key}',
+            )
+        )
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
---
+++ fv_profile.py
@@ -0,0 +1,230 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+from scipy.optimize import curve_fit
+from scipy.signal import find_peaks, peak_widths
+from sklearn.linear_model import LinearRegression
+from datetime import datetime as dt
+import jinja2
+
+
+def acceleration_func(t, MSS, tau):
+    """Small function used when fitting curve to each sprint effort"""
+    return MSS * (1 - np.e**(-t/tau))
+
+
+class fvProfile:
+
+    def __init__(self, data, name, bw):
+
+        self.df = data
+        self.sprints = []
+        self.sprint_dict = {}
+        self.lowess = sm.nonparametric.lowess
+        self.bw = bw 
+        self.name = name
+
+    def smooth_data(self, velocity_col='Velocity', time_col='Seconds', span=0.05):
+        """Using statmodels LOWESS function to create smoothed locally weighted
+        linear regression"""
+        self.df.rename(columns={velocity_col:'vel', time_col:'time'}, inplace=True)
+        smooth_velocity = self.lowess(self.df['vel'],
+                  self.df['time'],
+                  frac=span)
+        self.df['smooth_velocity'] = smooth_velocity[:,1]
+        plt.plot(self.df['time'], self.df['vel'])
+        plt.plot(self.df['time'], self.df['smooth_velocity'])
+
+    def find_peaks(self, rel_height=0.4):
+        """Using scipy signal module to find and extract peaks"""
+        min_peak_v = self.df['smooth_velocity'].max() * 0.7
+
+        peaks = find_peaks(self.df['smooth_velocity'], height=min_peak_v)
+        peak_size = peak_widths(self.df['smooth_velocity'], peaks[0], 0.90)
+        self.peaks = peaks
+        self.peak_details = peak_size
+
+    def extract_peaks(self):
+        """Takes the peak data and creates a list of dataframes containing
+        each individual sprint effort"""
+        for i in range(len(self.peaks[0])):
+            left, right = int(self.peak_details[2][i]), int(self.peak_details[3][i])
+            peak = self.df.iloc[left:right]
+            self.sprints.append(peak)
+
+
+
+    def extract_accel_phase(self):
+        """Takes each peak in the peak_list (list of sprint efforts), finds a start point and
+        plateau point in order to extract only the accel phase"""
+        for i, sprint in enumerate(self.sprints):
+            decel = sprint['vel'].max() - 0.4
+            # Work from end of sprint and find first point where the velocity is less than
+            end_index = np.argmax(sprint['vel'][::-1] > decel)
+            # Find start velcoity with threshold of 1.5m/s
+            moving_index = np.argmax(sprint['vel'] > 1.5)
+            # Find the first zero velocity point back from the moving index
+            start_index = np.argmin(sprint['vel'][moving_index:0:-1] > 0.5)
+            """NEED TEST CONDITION IF NO STATIC START"""
+            # Find the point of first movement where velocity > 0.3 and add to start_index
+            zero_index = moving_index - start_index
+            self.sprints[i] = sprint.iloc[zero_index:-end_index]
+
+
+
+    def model_sprint_data(self):
+        """Using method from R Shorts paackage, fit curve to estimate MSS and tau"""
+        for i, sprint in enumerate(self.sprints):
+            self.sprints[i]['time'] = self.sprints[i]['time'] - self.sprints[i]['time'].min()
+            # Fit curve to estimate MSS and tau. Need to look at bounding values
+            (MSS, tau), _ = curve_fit(acceleration_func, sprint['time'], sprint['vel'], bounds=([1.,0.5], [9.,3.]))
+            # Calc max acceleration
+            MAC = MSS/tau
+            # Calc relative max power
+            PMAX = (MSS * MAC) / 4
+            predicted_velocity = acceleration_func(sprint['time'], MSS, tau)
+
+            data = {'MSS': MSS,
+                   'tau': tau,
+                   'MAC': MAC,
+                   'PMAX': PMAX}
+
+            self.sprint_dict[f'sprint_{i}'] = data
+            self.sprints[i]['predicted_velocity'] = predicted_velocity
+
+
+
+    def calculate_data(self, sprint, sprint_data, time_delay=0):
+        """Helper function to be used when iterating through each sprint -  adds new parameters to each sprint_dict"""
+        sprint['acceleration'] = sprint_data['MSS'] / sprint_data['tau'] * np.e**((-sprint['time']- time_delay)/sprint_data['tau'])
+        sprint['position'] = sprint_data['MSS'] * (sprint['time']  + sprint_data['tau'] * np.e**((-sprint['time']-time_delay)/sprint_data['tau'])) - sprint_data['MSS'] * sprint_data['tau']
+        sprint['Fhzt'] = self.bw * sprint['acceleration']
+        sprint['Fair'] = 0.33 * sprint['predicted_velocity']**2
+        sprint['Ftot'] = sprint['Fhzt'] + sprint['Fair']
+        sprint['Ftot_kg'] = sprint['Ftot'] / self.bw
+        sprint['PowerHzt_kg'] = sprint['Ftot_kg'] * sprint['predicted_velocity']
+        sprint['Fres'] = np.sqrt(sprint['Fhzt']**2 + (self.bw * 9.81)**2)
+        sprint['RF_perc'] = np.where(sprint['time'] < 0.3, 0, sprint['Fhzt']/sprint['Fres'])
+        sprint['name'] = self.name
+        sprint['self.bw'] = self.bw
+
+        return sprint
+
+
+
+    def apply_calculations(self):
+        """Pass each sprint and its associated parameters to the caluclate_data function"""
+        for i, (sprint, sprint_data) in enumerate(zip(self.sprints, self.sprint_dict.keys())):
+            self.sprints[i] = self.calculate_data(sprint, self.sprint_dict[sprint_data])
+
+
+
+
+    def model_parameters(self, sprint, sprint_data):
+        """Helper function to calculate further sprint parameters including: F0, V0, Pmax"""
+        F0_model = LinearRegression()
+        F0_model.fit(sprint['predicted_velocity'].values.reshape(-1,1), sprint['Ftot'].values.reshape(-1,1))
+        sprint_data['F0'] = F0_model.intercept_[0]
+        F0_kg_model = LinearRegression()
+        F0_kg_model.fit(sprint['predicted_velocity'].values.reshape(-1,1), sprint['Ftot_kg'].values.reshape(-1,1))
+        sprint_data['F0_kg'] = sprint_data['F0'] / self.bw
+        sprint_data['FV_profile'] = F0_kg_model.coef_[0][0]
+        sprint_data['V0'] = -sprint_data['F0_kg'] / sprint_data['FV_profile']
+        sprint_data['Pmax'] = sprint_data['F0'] * sprint_data['V0'] / 4
+        sprint_data['Pmax_kg'] = (sprint_data['Pmax'] / self.bw)
+        sprint_data['RF_max'] = np.max(sprint['RF_perc'])
+        DRF_kg_model = LinearRegression()
+        DRF_kg_model.fit(sprint['predicted_velocity'].values.reshape(-1,1), sprint['RF_perc'].values.reshape(-1,1))
+        sprint_data['DRF'] = DRF_kg_model.coef_[0][0]
+
+        return sprint_data
+
+
+
+    def calculate_params(self):
+        """"Pass each sprint and its associated parameters to the caluclate_parameters function"""
+        for i, (sprint, sprint_data) in enumerate(zip(self.sprints, self.sprint_dict.keys())):
+            self.sprint_dict[sprint_data] = self.model_parameters(sprint, self.sprint_dict[sprint_data])
+
+
+    def plot_single_plot(self, sprint, sprint_data, plot_title=None, plot_name=None):
+        """helper funtion to plot a series of subplots graphing the sprint data"""
+
+        fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(24,12))
+        fig.suptitle(f"{plot_title} - {self.name}", fontsize="xx-large")
+
+        axcolor1 = "tab:red"
+        axcolor2 = "tab:blue"
+        axes[0,0].grid(True, alpha=0.3)
+        axes[0,0].set_title("Force & Speed Profile")
+        #axes[0,0].set_ylim(0,5) # NEED TO SET BASED OFF PROPER DATA
+        axes[0,0].scatter(sprint['time'], sprint['vel'], c='black')
+        axes[0,0].plot(sprint['time'], sprint['predicted_velocity'], c='red')
+        axes[0,0].set_xlabel('Time (s)', )
+        axes[0,0].set_ylabel('Running Speed (m/s)', color=axcolor1)
+        axes0 = axes[0,0].twinx()
+        #axes0.grid(True)
+        #axes0.set_ylim(0,5)
+        axes0.plot(sprint['time'], sprint['acceleration'], c='blue')
+        axes0.set_ylabel('Acceleration (m/s)', color=axcolor2)
+
+
+        axcolor1 = "tab:red"
+        axcolor2 = "tab:gray"
+        axes[0,1].grid(True, alpha=0.3)
+        axes[0,1].set_title("Force & Power Profile")
+        axes[0,1].plot(sprint['time'], sprint['predicted_velocity'], c='red')
+        axes[0,1].plot(sprint['time'], sprint['Ftot_kg'], c='gray')
+        axes[0,1].set_xlabel("Time (s)")
+        axes[0,1].set_ylabel("Force (N/kg) and Speed (m/s)", color=axcolor1)
+        axes1 = axes[0,1].twinx()
+        axes1.plot(sprint['time'], sprint['PowerHzt_kg'], c='black')
+        axes1.set_ylabel("Power (W/kg)", color=axcolor2)
+
+        axcolor1 = "tab:gray"
+        axes[1,0].grid(True, alpha=0.3)
+        axes[1,0].set_title("Field Sprint Force-Velocity-Power Profile")
+        axes[1,0].plot(sprint['predicted_velocity'], sprint['Ftot_kg'], c='gray')
+        axes[1,0].set_xlabel("Velocity (m/s)")
+        axes[1,0].set_ylabel("Force (N/kg)", color=axcolor1)
+        axes2 = axes[1,0].twinx()
+        axes2.plot(sprint['predicted_velocity'], sprint['PowerHzt_kg'], c='black')
+        axes2.set_ylabel("Power (W/kg)", color=axcolor2)
+
+        axcolor1 = "tab:blue"
+        axes[1,1].grid(True, alpha=0.3)
+        axes[1,1].set_title("Horizontal Force Velocity Profiles")
+        axes[1,1].plot(sprint['predicted_velocity'][sprint['time'] > 0.3], sprint['RF_perc'][sprint['time'] > 0.3], c='blue')
+        axes[1,1].set_xlabel("Velocity (m/s)")
+        axes[1,1].set_ylabel("Ratio Force (%)", color=axcolor1)
+
+        plt.savefig(plot_name)
+
+
+    def plot_multiple_plots(self):
+        """Plot a series of subplots for each file and for each sprint"""
+        for i, (sprint, sprint_data) in enumerate(zip(self.sprints, self.sprint_dict.keys())):
+            file_name = f"{dt.now().strftime('%Y%m%d')}_{self.name}_{i}"
+            self.plot_single_plot(sprint, self.sprint_dict[sprint_data], plot_title=sprint_data, plot_name=file_name)
+            self.sprint_dict[sprint_data]["plot_name"] = file_name
+
+    def create_report(self, template="template.html"):
+        """Create a report using Jinja template"""
+        df_list = [{key: value} for key, value in self.sprint_dict.items()]
+        df = pd.DataFrame.from_dict(self.sprint_dict).T.drop("plot_name", axis=1)
+        df = df.astype('float').round(2)
+        plot_names = [self.sprint_dict[sprint]['plot_name'] for sprint in self.sprint_dict.keys()]
+
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(searchpath=''))
+        template = env.get_template(template)
+        html = template.render(data=df.style.set_precision(2).render(),
+                              #data=df.to_html(),
+                              player_name=self.name,
+                              plots=plot_names)
+
+        # Write the HTML file
+        with open(f'{dt.now().strftime("%Y%m%d")}_{self.name}.html', 'w') as f:
+            f.write(html)
+
+
